014d91797a5895b5542b43cfc159485c4265ed28
fastens <a614v07@gmail.com>
2026-06-07 21:57:30 -0500

    done with starter html & css

---
+++ calc/javascript.js
@@ -0,0 +1,43 @@
+function add(numOne, numTwo) {
+   return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+   return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+   return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+   return numOne / numTwo;
+}
+
+function operate(operator, numOne, numTwo) {
+   if (operator === '+') {
+      return add(numOne, numTwo);
+   }
+   else if (operator === '-')
+   {
+      return subtract(numOne, numTwo);
+   }
+   else if (operator === '*')
+   {
+      return multiply(numOne, numTwo);
+   }
+   else if (operator === '/')
+   {
+      return divide(numOne, numTwo);
+   }
+}
+
+// button text into display
+displayElement = document.querySelector(".displayElement");
+
+document.addEventListener("click", function(e) {
+   if (e.target.tagName === 'BUTTON') {
+      let buttonText = e.target.textContent;
+      displayElement.textContent = buttonText;
+   }
+});
